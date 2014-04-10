a5b456025048a9a6122044059279ea3fac1a2b58
Alumne <Alumne@192.168.16.31>
2014-04-10 11:04:48 +0200

    Versió 1.1 del projecte prjava01, afegir linia <h1>

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -10,7 +10,8 @@ public class Prjava01 {
 
     public static void main(String[] args) throws IOException {
         File f= new File("fitxer.html"); 
-	try(BufferedWriter bw=new BufferedWriter(new FileWriter(f))){ 
+	try(BufferedWriter bw=new BufferedWriter(new FileWriter(f))){
+           
 		bw.write("<html>");bw.newLine(); 
 		bw.write("<head>");bw.newLine(); 
 		bw.write("<title>");bw.newLine(); 
@@ -18,6 +19,7 @@ public class Prjava01 {
 		bw.write("</title>");bw.newLine(); 
 		bw.write("</head>");bw.newLine(); 
 		bw.write("<body>");bw.newLine(); 
+                bw.write("<h1>DAW2m08uf4pr2</h1>");bw.newLine(); 
 		bw.write("Novap&agrave;ginaweb");bw.newLine(); 
 		bw.write("</body>");bw.newLine(); 
 		bw.write("</html>");bw.newLine(); 
